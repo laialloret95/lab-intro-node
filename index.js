28e037963005a00140a7c010cc206714f400ca47
Laia Lloret <laialloret95@gmail.com>
2021-04-09 10:03:34 +0200

    define functions

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [1,2,3],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((sum, value) => sum + value);
+    return sum;
+  }
 
   avg() {}
 }
+
+let list = new SortedList();
+list.sum()
 
 module.exports = SortedList;
